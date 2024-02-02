2f8e6a8abaa5842e413672f8ed01afcbc945e5a9
DpoBacek <groodrincinart@gmail.com>
2024-02-02 17:27:30 +0300

    Update battle.cpp

--- battle/battle.cpp
+++ battle/battle.cpp
@@ -1,5 +1,6 @@
 ﻿#include <cmath>
 #include <conio.h>
+#include <cstdlib>
 #include <cstring>
 #include <iostream>
 #include <locale.h>
@@ -8,24 +9,44 @@
 #include <time.h>
 
 using namespace std;
+static int random(int min, int max);
+class Player;
+class Enemy;
+void fight(int player_act, int enemy_act, Enemy enemy, Player user);
+Enemy r_ememy();
+Player init();
 
-int random(int min, int max);
 
-class enemy {
+
+class Enemy {
 private:
+	int hp;
+	int damage;
 	string name;
-	int damage = 10;
+	int strength_armor;
 public:
-	int health;
-	friend class player;
-	enemy(string name, int health) {
-		this->name = name;
-		this->health = health;
+	int is_alive = true;
+	friend class Player;
+	friend void fight(int player_act, int enemy_act, Enemy enemy, Player user);
+	Enemy() {}
+	Enemy(int hp, int damage, int strength_armor, string name) {
+		set_info(hp, damage, strength_armor, name);
 	}
+	void show() {
+		cout << "У " << name << " " << hp << "hp" << " и его урон составляет " << damage << " едениц." << endl;
+	}
+	void set_info(int hp, int damage, int strength_armor, string name) {
+		this->damage = damage;
+		this->hp = hp;
+		this->strength_armor = strength_armor;
+		this->name = name;
+	}
+	void e_shield() {}
 };
 
-class player {
+class Player {
 private:
+	string class_hero;
 	int hp;
 	int damage_close;
 	string name;
@@ -33,37 +54,21 @@ private:
 	int hp_potion = 0;
 	int strength_armor = 0;
 public:
+	friend class Enemy;
+	friend void fight(int player_act, int enemy_act, Enemy enemy, Player user);
 	int total_score = 0;
 	int max_total_score = random(1300, 2000);
 	bool is_alive = true;
-	player(int hp, int damade, int money, string name) {
-		this->damage_close = damade;
+	Player(string class_hero, int hp, int damage, int money, int strength_armor, string hero_name) {
+		set_info(class_hero, hp, damage, money, strength_armor, hero_name);
+	}
+	void set_info(string class_hero, int hp, int damage, int money, int strength_armor, string hero_name) {
+		this->damage_close = damage;
 		this->hp = hp;
 		this->money = money;
-		this->name = name;
-	}
-	void damage(enemy& enemy) {
-		if (enemy.health < 11) {
-			cout << enemy.name << " умирает от рук " << name << endl; enemy.health -= damage_close;
-		}
-		else {
-			enemy.health -= damage_close;
-			cout << name << " наносит 10 урона " << enemy.name << endl;
-			cout << "У " << enemy.name << " " << enemy.health << " hp." << endl << endl;
-		}
-	}
-	void healing() {
-		if (hp_potion >= 1) {
-
-			if (hp == 100) { cout << name << " здоров." << endl << endl; }
-			else {
-				hp += 15;
-				hp_potion -= 1;
-				cout << " Вы лечите себя на 15 hp, теперь у вас " << hp << "." << endl;
-			}
-		}
-		else cout << "У вас нет зелий.";
-
+		this->class_hero = class_hero;
+		this->strength_armor = strength_armor;
+		name = hero_name;
 	}
 	void training() {
 		money -= 100;
@@ -75,6 +80,7 @@ public:
 	}
 	void show() {
 		cout << "Характеристики: " << endl
+			<< "Класс: " << class_hero << endl
 			<< "Здоровье: " << hp << endl
 			<< "Урон: " << damage_close << endl
 			<< "Баланс: " << money << endl
@@ -116,7 +122,6 @@ public:
 			total_score += random(0, 10);
 		}
 		if (hp < 0) is_alive = false;
-		if (strength_armor < 0) strength_armor += abs(strength_armor);
 
 
 
@@ -139,7 +144,7 @@ public:
 			if (money > 500) {
 				if (strength_armor >= 100) cout << "Ваша броня в идеальном состоянии.";
 				else {
-					strength_armor += (100);
+					strength_armor = (100);
 					money -= 500;
 					cout << "Вы купиле броню, у которой  " << strength_armor << " прочность и " << money << " монет." << endl;
 				}
@@ -148,30 +153,144 @@ public:
 		}
 		cout << "Вы покидаете магазин." << endl << endl;
 	};
-	void finish_stage() {}
+	void clear() {
+		system("cls");
+		cout << "Справочный материал нажав на:" << endl << "s - Вы отобразите характеристика " << endl << "t - Вы за 100 монет получите добавку к урону " << endl << "g - Вы сходите в гильдию и попытаетесь выполнить их задание " << endl << "m - Вы можете сходить в магазин купить зелье" << endl << "a - Вы можете сходить на арена " << endl << "h - Вы можете использовать зелье" << endl << "c - Очистка экрана" << endl << endl;
+
+	}
+	void healing() {
+		if (hp_potion >= 1) {
+			if (hp == 100) { cout << name << " здоров." << endl << endl; }
+			else {
+				hp += 25;
+				hp_potion -= 1;
+				cout << "Вы лечите себя на 15 hp, теперь у вас " << hp << "." << endl;
+			}
+		}
+		else cout << "У вас нет зелий.";
+	}
+	void arena(Player user) {
+		Enemy enemy = r_ememy();
+		int p = 0;
+		int r_damage_a = random(5, 25);
+		cout << "Вы попадаете на арену и вашим врагом стоноваться " << enemy.name << endl;
+		cout << "Ваши действия?" << endl << "a - Нанести урон врагу" << endl << "s - Спрятаться за щитом" << endl << "h - Использовать зелье" << endl << "q - Попытаться сбежать" << endl << endl;
+		enemy.show();
+
+		while (enemy.is_alive <= 5) { // if (enemy.hp < 0) return;
+			cout << endl << "Что вы предпримите? ";
+			int userInput_arena = _getch();
+			cout << endl;
+			switch (userInput_arena) {
+			case 97: fight(0, damage(enemy), enemy, user); break; // атака по врагу 
+			case 104:fight(2, damage(enemy), enemy, user); ; break; // лечение 
+			case 115:fight(1, damage(enemy), enemy, user); break;
+			case 113: cout << "Вы смогли сбежать с арены, но всё прошло не так гладко...\nВы получили " << r_damage_a << " урона."; hp -= r_damage_a; return;
+
+			}
+
+		}
+		total_score += random(50, 130);
+		cout << "Вы покинули арену." << endl << endl;
+	};
+	int damage(Enemy enemy) { // урона по игроку (0 - нанесение урона, 1 - ипользование щита)
+		if (random(1, 4) == 4) { return 1; }
+		else { return 0; }
+	}
+	int e_damage(Enemy enemy) { // урон по врагу (0 - нанесение урона, 1 - ипользование щита)
+		return 0;
+	}
+	int shield() {
+		return 1;
+	}
 };
-int random(int min, int max) {
+
+
+
+
+
+
+static int random(int min, int max) {
 	random_device rd;
 	mt19937 gen(rd());
 	uniform_int_distribution<> random_num(min, max);
 	return random_num(gen);
 }
 
+Player init() {
+	string hero_name;
+	int i = 0;
+	Player user("0", 0, 0, 0, 0, "0");
+	cout << "Придумайте себе имя: "; cin >> hero_name;
+	cout << endl << "\tВыберите класс" << endl << "\ta - Assissin " << endl << "\tw - Warrior " << endl << "\tp - Paladin " << endl;
+	while (i <= 3) {
+		int userInput_class = _getch();
+		i++;
+		if (userInput_class == 97) { user.set_info("Assissin", 500, 20, 100, 0, hero_name); system("cls"); cout << "Вы выбрали класс Assissin" << endl << endl; break; }
+		else if (userInput_class == 119) { user.set_info("Warrior", 100, 12, 100, 0, hero_name); system("cls"); cout << "Вы выбрали класс Warrior" << endl << endl; break; }
+		else if (userInput_class == 112) { user.set_info("Paladin", 80, 10, 100, 100, hero_name); system("cls"); cout << "Вы выбрали класс Paladin" << endl << endl; break; }
+		else if (userInput_class == 103) { user.set_info("God", 10000000, 100, 10000000, 100, hero_name); system("cls"); cout << "Вы выбрали класс God" << endl << endl; break; }
+		else { cout << "Введено недопустимое действие!" << endl; }
+		if (i == 2) user.set_info("Peasant", 50, 5, 10, 0, hero_name);
+	}
+	return user;
+}
+Enemy r_ememy() {
+	Enemy enemy(0, 0, 0, "0");
+	int r_ememy = random(1, 10);
+	switch (r_ememy) {
+	case 1:enemy.set_info(200, 20, 0, "Тигр"); break;
+	case 2:enemy.set_info(100, 10, 50, "Бандит"); break;
+	case 3:enemy.set_info(100, 10, 70, "Бандит"); break;
+	case 4:enemy.set_info(100, 8, 10, "Бандит"); break;
+	case 5:enemy.set_info(100, 50, 150, "Герой"); break;
+	case 6:enemy.set_info(50, 10, 0, "Волк"); break;
+	case 7:enemy.set_info(100, 25, 100, "Гладиатор"); break;
+	case 8:enemy.set_info(70, 12, 0, "Волк-вожак"); break;
+	case 9:enemy.set_info(30, 9, 0, "Ослабевший волк"); break;
+	case 10:enemy.set_info(100, 15, 100, "Главарь бандитов"); break;
+	}
+	return enemy;
+}
+void fight(int player_act, int enemy_act, Enemy enemy, Player user) {
+	if (player_act == 0 && enemy_act == 0) {
+		if (enemy.hp <= user.damage_close) { enemy.hp -= user.damage_close; cout << "Вы выйграли этот бой. " << enemy.hp << endl; return; }
+		else if (enemy.hp > user.damage_close) {
+			enemy.hp -= user.damage_close;
+			cout << "Вы нанесли " << user.damage_close << " урона врагу, у врага остольсь " << enemy.hp << " hp." << endl;
+		}
+		user.hp -= enemy.damage; cout << "Вы получили " << enemy.damage << " урона от " << enemy.name << ", у вас осталось " << user.hp << " hp." << endl;
+	}
+	if (player_act == 1 && enemy_act == 0) { cout << "Вы успешно парируете удар щитом"; }
+	if (player_act == 0 && enemy_act == 1) { cout << "Вы попытались ударить но ваш противние парировал удар шитом. "; }
+	if (player_act == 1 && enemy_act == 1) { cout << "Вы пробуете защититься щитом ... как и ваш противник."; }
+	if (player_act == 2) { user.hp -= enemy.damage; cout << "Противник замечает что вы пытаетесь отхалиться, вы получили " << enemy.damage << " урона от " << enemy.name << ", у вас осталось " << user.hp << " hp." << endl; }
+	if (user.hp < 0) return;
+
+}
+
+
+
+
+
 int main() {
 	int k = 0;
 	setlocale(LC_ALL, "RU");
-	player user(100, 10, 100, "Ivan"); // ввод данных о персонажа
-	enemy pav("Pavel", 100);
-	cout << "Справочный материал нажав на:" << endl << "s - Вы отобразите характеристика " << endl << "t - Вы за 100 монет получите добавку к урону " << endl << "g - Вы сходите в гильдию и попытаетесь выполнить их задание " << endl << "m - Вы можете сходить в магазин купить зелье" << endl << "h - Вы можете использовать зелье" << endl << endl;
+	cout << "Противник замечает что вы пытаетесь отхалиться, вы получили  10" << " урона от  a,sd,as" << ", у вас осталось  10" << " hp." << endl;
+	Player user = init();
+	Enemy enemy = r_ememy();
+
+	cout << "Справочный материал нажав на:" << endl << "s - Вы отобразите характеристика " << endl << "t - Вы за 100 монет получите добавку к урону " << endl << "g - Вы сходите в гильдию и попытаетесь выполнить их задание " << endl << "m - Вы можете сходить в магазин купить зелье" << endl << "a - Вы можете сходить на арена " << endl << "h - Вы можете использовать зелье" << endl << "c - Очистка экрана" << endl << endl;
 	while (user.is_alive) {
 		int userInput = _getch();
 		switch (userInput) {
 		case 104:user.healing(); break;
-		case 100:user.damage(pav); break;
 		case 115:user.show(); break;
 		case 116:user.training(); break;
 		case 103:user.guild(); break;
 		case 109:user.shop(); break;
+		case 99:user.clear(); break;
+		case 97:user.arena(user); break;
 		default:cout << "Введено недопустимое действие!" << endl; break;
 		}
 		if (user.total_score >= user.max_total_score && k == 0) k++;
@@ -183,13 +302,14 @@ int main() {
 
 	}
 	cout << endl << "Вы мертвы...";
+
+
 }
 
-// АРЕНА должен рандомить защить-атака (шанс на оглушение) (шанс контр атаки после шита x 1.0 - x 1.3)
-// СИСТЕМА КЛАССОВ
+
+// АРЕНА должен рандомить защить-атака (шанс на оглушение) (шанс контр атаки после шита x 1.0 - x 1.3) !!!!! добавить смерть
 // total score (1300, 2000) k =1  = действия  должны влиять на него  как только ~> 1000 мы выводим "Знатные люди говорят о вас."  -> любое действие "На вас открыта охота." ->
 // -> Открываеться босс 
-// Щит всем! 
+
+// Щит всем! !!!!реализация!!!!
 // рандомизация однотипного текста
-
-
